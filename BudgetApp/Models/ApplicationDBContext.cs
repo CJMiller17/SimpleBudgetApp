1dc36bf7b30e639d2009d16db40339e721ff2257
Cody <c.jmiller17@yahoo.com>
2025-03-19 14:54:39 -0400

    Initial Commit

---
+++ BudgetApp/Models/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetApp.Models
+{
+  public class ApplicationDbContext:DbContext
+  {
+    public ApplicationDbContext(DbContextOptions options):base(options)
+    {}
+    
+    public DbSet<Transaction> Transactions { get; set; }
+    
+    public DbSet<Category> Categories { get; set; }
+  }
+ 
+}
+
