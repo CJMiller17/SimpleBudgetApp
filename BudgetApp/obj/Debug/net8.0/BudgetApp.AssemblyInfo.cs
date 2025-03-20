d137cad2a3df566a48990b087d0d6dd863230fae
Cody <c.jmiller17@yahoo.com>
2025-03-20 00:47:51 -0400

    Added a dashboard to view balances.

--- BudgetApp/obj/Debug/net8.0/BudgetApp.AssemblyInfo.cs
+++ BudgetApp/obj/Debug/net8.0/BudgetApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b10b6080b9cf959246cd5f51efca13ec62f6edf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+990fe367218c0e8fcadb6be36ffd61a25318d9b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
